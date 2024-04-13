96c15d622cc1bae9c331d8d666b9e3994c173514
bshongwe <dru.da.beat.hustle@gmail.com>
2024-04-13 22:30:51 +0200

    App main entry point: import modules, app pub class

---
+++ src/main/java/com/example/fileuploadapi/FileUploadAPIApplication.java
@@ -0,0 +1,14 @@
+// Ernest B. Shongwe
+// Spring Boot App: Main Entry Point
+package com.example.fileuploadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadApiApplication.class, args);
+    }
+}
