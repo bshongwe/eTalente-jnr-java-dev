f88bc0fa5823fe3bdc3a0ca30f6ffaacc14a45f7
bshongwe <dru.da.beat.hustle@gmail.com>
2024-04-13 23:13:57 +0200

    Enable Swagger

--- src/main/java/com/example/fileuploadapi/FileUploadAPIApplication.java
+++ src/main/java/com/example/fileuploadapi/FileUploadAPIApplication.java
@@ -4,8 +4,11 @@ package com.example.fileuploadapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Enable Swagger2
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FileUploadApiApplication {
 
     public static void main(String[] args) {
